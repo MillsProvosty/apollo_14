595894ca4546e7f6db64caa1611c9300e846845c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-10 10:01:49 -0600

    So many bugsgit status! only finished user story one.

---
+++ spec/features/astronaut_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit astronauts'do
+  describe 'I see a list of astronauts with the following info' do
+    before :each do
+      @astronaut_1 = Astronaut.create!(name: "Neil", age: 34, job: "Pilot")
+    end
+
+    it 'shows name, age, job' do
+      save_and_open_page
+      visit astronauts_path
+      expect(page).to have_content(@astronaut_1.name)
+      expect(page).to have_content(@astronaut_1.age)
+      expect(page).to have_content(@astronaut_1.job)
+
+    end
+
+    it 'shows average age of all astronauts' do 
+    # As a visitor,
+    # When I visit '/astronauts'
+    # I see the average age of all astronauts.
+    #
+    # (e.g. "Average Age: 34")
+
+
+  end
+end
